078e9fbd1a5465f152ed0e4b738ffb24db7acff7
Unknown <sgjef1@student.monash.edu>
2017-04-22 18:09:34 +0200

    Creating Moniutor Classes

---
+++ WeatherMonitor/WeatherMonitor/MonitorCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitor
+{
+    class MonitorCollection
+    {
+        private MonitorFactory[] locationArray;
+
+        public MonitorCollection(string[] input)
+        {
+            for (int i = 0; i <= input.Length; i++)
+            {
+                MonitorFactory temp = new MonitorFactory(input[i]);
+            }
+        }
+
+    }
+
+   
+}
---
+++ WeatherMonitor/WeatherMonitor/MonitorFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitor
+{
+    class MonitorFactory
+    {
+        string locationName;
+        string timeStamp;
+
+        public MonitorFactory(String locationName)
+        {
+            locationName = this.locationName;
+            timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+        }
+    }
+}

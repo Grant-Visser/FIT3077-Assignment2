d878c26007671e84693e9d6213b4fa4d185155f2
Grant Visser <grantrvisser@gmail.com>
2017-04-22 17:25:09 +0200

    Creating Location Classes
    Just Skeleton Code for now

---
+++ WeatherMonitor/WeatherMonitor/LocationFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitor
+{
+    class LocationFactory
+    {
+        string locationName;
+        string timeStamp;
+
+        public LocationFactory(String locationName, String timeStamp)
+        {
+            locationName = this.locationName;
+            timeStamp = this.timeStamp; 
+        }
+    }
+}

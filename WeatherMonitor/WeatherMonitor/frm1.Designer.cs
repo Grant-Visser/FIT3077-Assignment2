3047e759c16864067ab609ad3b8ec053698e4fb2
Unknown <sgjef1@student.monash.edu>
2017-04-28 20:00:36 +0200

    Moved buttons to correct alignments

--- WeatherMonitor/WeatherMonitor/frm1.Designer.cs
+++ WeatherMonitor/WeatherMonitor/frm1.Designer.cs
@@ -116,7 +116,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(99)))), ((int)(((byte)(234)))));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(754, 8);
+            this.pictureBox2.Location = new System.Drawing.Point(762, 4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(25, 25);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;

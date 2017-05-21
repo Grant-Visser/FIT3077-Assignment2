3073bda9e12995590b851a1707716dd0fa7ee737
Unknown <sgjef1@student.monash.edu>
2017-05-21 17:36:48 +0200

    Graphing input: In progress

--- WeatherMonitor/WeatherMonitor/MonitorTemplate.cs
+++ WeatherMonitor/WeatherMonitor/MonitorTemplate.cs
@@ -7,12 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
 
 namespace WeatherMonitor
 {
+    public class MeasureModel
+    {
+        public System.DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
     public partial class MonitorTemplate : Form
     {
         private MonitorFactory mf { get; set; }
+        public ChartValues<MeasureModel> ChartValues { get; set; }
+        public ChartValues<MeasureModel> ChartValues2 { get; set; }
+
         public MonitorTemplate()
         {
             InitializeComponent();
@@ -24,6 +35,54 @@ namespace WeatherMonitor
             InitializeComponent();
             mf = input;
             this.Show();
+            // this is temp measuremodel input <<>>
+            var mapper = Mappers.Xy<MeasureModel>()
+               .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+               .Y(model => model.Value);           //use the value property as Y
+
+            //lets save the mapper globally.
+            Charting.For<MeasureModel>(mapper);
+
+            //the ChartValues property will store our values array
+            ChartValues = new ChartValues<MeasureModel>();
+            ChartValues2 = new ChartValues<MeasureModel>();
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = ChartValues,
+                    PointGeometrySize = 18,
+                    StrokeThickness = 4
+                },
+                new LineSeries
+                {
+                    Values = ChartValues2,
+                    PointGeometrySize = 18,
+                    StrokeThickness = 4
+                }
+
+            };
+
+
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                DisableAnimations = true,
+                LabelFormatter = value => new System.DateTime((long)value).ToString("mm:ss"),
+                Separator = new Separator
+                {
+                    Step = TimeSpan.FromSeconds(20).Ticks
+                }
+            });
+
+            SetAxisLimits(System.DateTime.Now);
+
+
+        }
+
+        private void SetAxisLimits(System.DateTime now)
+        {
+            cartesianChart1.AxisX[0].MaxValue = now.Ticks + TimeSpan.FromSeconds(400).Ticks; // lets force the axis to be 100ms ahead
+            cartesianChart1.AxisX[0].MinValue = now.Ticks - TimeSpan.FromSeconds(400).Ticks; //we only care about the last 8 seconds*/
         }
 
         private void MonitorTemplate_Load(object sender, EventArgs e)
@@ -45,7 +104,23 @@ namespace WeatherMonitor
             mf.update();
             gaugeRainfall.Value = Convert.ToDouble(mf.Rain[2]);
             gaugeTemp.Value = Convert.ToDouble(mf.Temp[1]);
-            txtOut.Text += "Update @" + mf.UpdateTimeStamp + " - Temperature: " + mf.Temp[1] + "°C - Rainfall: " + mf.Rain[2]+ "mm" +Environment.NewLine + "=======================" + Environment.NewLine; 
+            txtOut.Text += "Update @" + mf.UpdateTimeStamp + " - Temperature: " + mf.Temp[1] + "°C - Rainfall: " + mf.Rain[2]+ "mm" +Environment.NewLine + "=======================" + Environment.NewLine;
+
+            var now = System.DateTime.Now;
+
+            ChartValues.Add(new MeasureModel
+            {
+                DateTime = now,
+                Value = Convert.ToDouble(mf.Rain[2])
+            });
+
+            ChartValues2.Add(new MeasureModel
+            {
+                DateTime = now,
+                Value = Convert.ToDouble(mf.Temp[1])
+            });
+
+            SetAxisLimits(now);
         }
     }
 }
